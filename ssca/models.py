3410435f6bb45f204ddca0e935089e67d88a5fb3
Reuben Firmin <rfirmin@catalist.us>
2013-01-06 08:40:14 -0500

    model location

--- ssca/models.py
+++ ssca/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Subscription(models.Model):
     start_date = models.DateField()
     end_date = models.DateField()
-    amount_paid = models.DecimalField(max_digits=3, decimal_places=2)
+    amount_paid = models.DecimalField(max_digits=5, decimal_places=2)
     date_paid = models.DateField()
 
 class Account(models.Model):
@@ -15,3 +15,10 @@ class Person(models.Model):
     user = models.ForeignKey(User)
     # openid identity string, used to find which User has logged in
     identity = models.TextField()
+
+class Location(models.Model):
+    date = models.DateField()
+    latitude = models.DecimalField(max_digits=8, decimal_places=5)
+    longitude = models.DecimalField(max_digits=7, decimal_places=5)
+    person = models.ForeignKey(Person)
+
