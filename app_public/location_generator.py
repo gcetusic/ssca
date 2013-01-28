e0f6886e562190ad1651c39df4de1cef29d58764
Goran Cetušić <goran.cetusic@gmail.com>
2013-01-28 11:23:46 +0100

    optimized random location generator, now takes seconds instead of minutes

--- app_public/location_generator.py
+++ app_public/location_generator.py
@@ -1,18 +1,20 @@
 from .models import Location
 from datetime import datetime
+from django.db.models.query import QuerySet
 import decimal
 import random
 
 
-def gen_random_position():
-    latitude = decimal.Decimal('%d.%d' % (random.randint(-89, 89), random.randint(0, 99999)))
-    longitude = decimal.Decimal('%d.%d' % (random.randint(-179, 179), random.randint(0, 99999)))
+def gen_random_position(latrange, lonrange):
+    latitude = decimal.Decimal('%d.%d' % (random.randint(latrange[0], latrange[1]), random.randint(0, 99999)))
+    longitude = decimal.Decimal('%d.%d' % (random.randint(lonrange[0], lonrange[1]), random.randint(0, 99999)))
 
     return (latitude, longitude)
 
 
-def write_data(limit=10000):
+def write_data(latrange=(-89, 89), lonrange=(-179, 179), limit=10000):
+    locations = []
     for i in range(limit):
-        position = gen_random_position()
-        location = Location(date=datetime.now(), latitude=position[0], longitude=position[1])
-        location.save()
+        position = gen_random_position(latrange, lonrange)
+        locations.append(Location(date=datetime.now(), latitude=position[0], longitude=position[1]))
+    Location.objects.bulk_create(locations)
