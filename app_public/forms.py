a5e16f8b797d2575fb74feb03a35d33576cb8240
Navjot Singh <weavebytes@gmail.com>
2013-02-01 00:26:15 +0530

    SSCA-124: adding card types in form

--- app_public/forms.py
+++ app_public/forms.py
@@ -4,6 +4,14 @@ class SSCAJoinForm(forms.Form):
     """
     SSCA Join Form defines fields to be used in Join SSCA js popups.
     """
+
+    card_types = [('Visa', 'Visa'), 
+            ('MasterCard', 'MasterCard'),
+            ('American Express', 'American Express'),
+            ('Discover', 'Discover'),
+            ('Diners Club', 'Diners Club'),
+            ('JCB', 'JCB')]
+
     # for step 1 dialog
     firstname = forms.CharField(max_length=32, widget=forms.TextInput(
         attrs={'placeholder': 'First Name'}))
@@ -17,7 +25,7 @@ class SSCAJoinForm(forms.Form):
 
     # for step 3 dialog
     name_on_card = forms.CharField(max_length=32)
-    card_type = forms.CharField(max_length=32)
+    card_type = forms.ChoiceField(widget = forms.Select(), choices = card_types, required = True,)
     card_number = forms.CharField(max_length=32)
     card_expiry_date = forms.DateField()
     card_csv = forms.CharField(max_length=3)
