827d05075a850d901f8b03c3934a857938327b29
Navjot Singh <weavebytes@gmail.com>
2013-02-01 01:41:36 +0530

    SSCA-121: specifying required fields in form

--- app_public/forms.py
+++ app_public/forms.py
@@ -4,9 +4,7 @@ class SSCAJoinForm(forms.Form):
     """
     SSCA Join Form defines fields to be used in Join SSCA js popups.
     """
-
-    card_types = [('Visa', 'Visa'), 
-            ('MasterCard', 'MasterCard'),
+    card_types = [('Visa', 'Visa'), ('MasterCard', 'MasterCard'),
             ('American Express', 'American Express'),
             ('Discover', 'Discover'),
             ('Diners Club', 'Diners Club'),
@@ -20,26 +18,33 @@ class SSCAJoinForm(forms.Form):
             ('25', 'Medium (12" x 18")'),
             ('17', 'Small (10" x 15")')]
 
-    # for step 1 dialog
-    firstname = forms.CharField(max_length=32, widget=forms.TextInput(
-        attrs={'placeholder': 'First Name'}))
-    lastname = forms.CharField(max_length=32, widget=forms.TextInput(
-        attrs={'placeholder': 'Last Name'}))
-    email = forms.EmailField( widget=forms.TextInput(
-        attrs={'placeholder': 'Email Address'}))
+    # fields for step 1 dialog
+    firstname = forms.CharField(max_length=32, required = True,
+            widget=forms.TextInput( attrs={'placeholder': 'First Name'}))
 
-    # for step 2 dialog
+    lastname = forms.CharField(max_length=32, required = True,
+            widget=forms.TextInput( attrs={'placeholder': 'Last Name'}))
+
+    email = forms.EmailField(required = True,
+            widget=forms.TextInput( attrs={'placeholder': 'Email Address'}))
+
+    # fields for step 2 dialog
     membership_fee = forms.IntegerField()
+
     mail_location = forms.ChoiceField(choices = mail_locations,
             widget=forms.Select(attrs={'onchange':'updateTotal();'}))
 
-    # for step 3 dialog
+    # fields for step 3 dialog
     burgee_type = forms.ChoiceField(choices = burgee_types,
             widget=forms.Select(attrs={'onchange':'updateSSCAPurchase();'}))
-    name_on_card = forms.CharField(max_length=32)
-    card_type = forms.ChoiceField(widget = forms.Select(), choices = card_types, required = True,)
-    card_number = forms.CharField(max_length=32)
-    card_expiry_date = forms.DateField()
-    card_csv = forms.CharField(max_length=3)
+
+    name_on_card = forms.CharField(max_length=32, required = True)
+
+    card_type = forms.ChoiceField(required = True, 
+            widget = forms.Select(), choices = card_types)
+
+    card_number = forms.CharField(max_length=32, required = True)
+    card_expiry_date = forms.DateField(required = True)
+    card_csv = forms.CharField(max_length=3, required = True)
     total_fee = forms.IntegerField() 
     yearly_reniew = forms.BooleanField() 
