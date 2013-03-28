d16f8ee806d1060e447a087cd962f8e7514fdf50
gcetusic <goran.cetusic@gmail.com>
2013-03-28 21:05:32 +0100

    added click event to all menu links

--- staticfiles/js/page.js
+++ staticfiles/js/page.js
@@ -1,6 +1,15 @@
+$(document).ready(function () {
+	$('.dropdown-menu a').attr('tabindex', -1);
+	$('.dropdown-menu a').click(function () {
+		Dajaxice.app_public.sscapage_ajax(load_page, {'page': this.getAttribute('href')});
+		return false;
+	});
+})
+
+
 function load_page(page) {
 	$('#page_title').html(page.title);
 	$('#page_content').html(page.content);
-	console.log(page.picture);
 	$('#page_picture').html(page.picture);
+	$('#page_picture > img').addClass('img-rounded');
 }
