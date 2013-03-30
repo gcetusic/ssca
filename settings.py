1232de996f88fe65478e647993c8009f88e6597d
Reuben Firmin <rfirmin@catalist.us>
2013-03-30 09:48:31 -0400

    workaround mysterious social_auth bug on server
    Conflicts:
    	settings.py

--- settings.py
+++ settings.py
@@ -190,3 +190,29 @@ elif deployment_env == 'stag':
 else:
     print('loading local config override')
     from settings_dev_wb import *
+
+####################
+# DYNAMIC SETTINGS #
+####################
+
+# set_dynamic_settings() will rewrite globals based on what has been
+# defined so far, in order to provide some better defaults where
+# applicable. We also allow this settings module to be imported
+# without Mezzanine installed, as the case may be when using the
+# fabfile, where setting the dynamic settings below isn't strictly
+# required.
+try:
+    from mezzanine.utils.conf import set_dynamic_settings
+except ImportError:
+    pass
+else:
+    set_dynamic_settings(globals())
+
+########################
+# SOCIAL AUTH SETTINGS #
+########################
+
+SOCIAL_AUTH_UID_LENGTH = 222
+SOCIAL_AUTH_NONCE_SERVER_URL_LENGTH = 200
+SOCIAL_AUTH_ASSOCIATION_SERVER_URL_LENGTH = 135
+SOCIAL_AUTH_ASSOCIATION_HANDLE_LENGTH = 125
