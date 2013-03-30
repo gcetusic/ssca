ba69c4f2a751fdbf4383edbca4c9eaaf530a8bb5
ostap <ostap_mal@hotmail.com>
2013-03-30 20:07:46 +0200

    add ajax

--- app_public/ajax.py
+++ app_public/ajax.py
@@ -1,6 +1,7 @@
 import json
 from dajaxice.decorators import dajaxice_register
 from app_public.views import sscapage
+import re
 
 
 @dajaxice_register(method='GET')
@@ -38,3 +39,19 @@ def get_public(request, id):
 @dajaxice_register(method='GET')
 def sscapage_ajax(request, page=None):
     return sscapage(request, page)
+
+@dajaxice_register(method='GET')
+def validate_location(request,location=None):
+    if location == None:
+        return json.dumps({'status': 'error'})
+    pattern = re.compile('^[N|S|n|s](\d{2}) (\d{2}.\d{2}), [W|E|w|e](\d{3}) (\d{2}.\d{2})$')
+    m = pattern.match(location)
+    if (m != None):
+            w1 = int(m.groups(0)[0])
+            w2 = float(m.groups(0)[1])
+            n1 = int(m.groups(0)[2])
+            n2 = float(m.groups(0)[3])
+            if (0 < w1 < 90) and (0 < w2 < 60) and (0 < n1 < 180) and (0 < n2 < 60):
+                return json.dumps({'status': 'success'})
+    return json.dumps({'status': 'error'})
+
