8748decc52b45de51a512ab34b03046dc2f39584
Reuben Firmin <rfirmin@catalist.us>
2013-04-13 09:50:05 -0400

    remove old settings section:

--- settings.py
+++ settings.py
@@ -186,23 +186,6 @@ elif deployment_env == 'dev':
     except ImportError:
         pass
 
-####################
-# DYNAMIC SETTINGS #
-####################
-
-# set_dynamic_settings() will rewrite globals based on what has been
-# defined so far, in order to provide some better defaults where
-# applicable. We also allow this settings module to be imported
-# without Mezzanine installed, as the case may be when using the
-# fabfile, where setting the dynamic settings below isn't strictly
-# required.
-try:
-    from mezzanine.utils.conf import set_dynamic_settings
-except ImportError:
-    pass
-else:
-    set_dynamic_settings(globals())
-
 ########################
 # SOCIAL AUTH SETTINGS #
 ########################
